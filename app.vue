b6d88d843fdb42c1b6e0455bf471c638fcea2a3d
Hessel <49622357+Heesel@users.noreply.github.com>
2024-02-27 10:41:46 +0100

    Vercel insights

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="font-kanit flex flex-col h-screen justify-between bg-gray-900 overflow-auto">
-    <LayoutsNavigation />
-    <NuxtPage />
-    <LayoutsTheFooter/>
-  </div>
+    <SpeedInsights />
+    <div class="font-kanit flex flex-col h-screen justify-between bg-gray-900 overflow-auto">
+        <LayoutsNavigation />
+        <NuxtPage />
+        <LayoutsTheFooter />
+    </div>
 </template>
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
+</script>
