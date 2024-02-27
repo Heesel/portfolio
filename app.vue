01f445612bfb2a1dc81f070350d0118946a1909c
Hessel <49622357+Heesel@users.noreply.github.com>
2024-02-27 10:59:32 +0100

    Vercel insight attempt #2

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
 <template>
-    <SpeedInsights />
-    <div class="font-kanit flex flex-col h-screen justify-between bg-gray-900 overflow-auto">
+    <div>
         <LayoutsNavigation />
         <NuxtPage />
         <LayoutsTheFooter />
     </div>
 </template>
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
 </script>
+<style>
+    body {
+        background-color: #111827;
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="font-kanit flex flex-col h-screen justify-between bg-gray-900 overflow-auto">
+        <SpeedInsights />
+        <slot/>
+    </div>
+</template>
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
+</script>
